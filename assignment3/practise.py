7018a5674643a7c08283d4c99e0bd5e29526ea4a
SrdXIV <52273083+SrdXIV@users.noreply.github.com>
2019-07-17 20:55:31 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,12 @@
+#1
+my_list = [9,8,7,6,5,5,4,3,2,1,50]
+print(sum(my_list));
+
+#2
+my_tuble = (1248)
+print(my_tuble);
+
+#3
+set1 = {1234,1248,4585,4589}
+set2 = {1248,4589,2345,4852}
+print(set1.intersection(set2));
