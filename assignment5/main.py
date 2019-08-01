ae0ee99caa5687abbc02a281bd5b570406b54943
Your Name <>
2019-08-01 16:04:18 +0700

    update

--- assignment5/main.py
+++ assignment5/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route('/', methods = ["POST"])
+def register():
+    return 'xyz';
 
 if __name__ == "__main__":
     app.run()
